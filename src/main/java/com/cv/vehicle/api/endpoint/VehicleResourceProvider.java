9d47ebae77698580882fd01954cbbfa26d587d8f
Ashish Wilson Nadar <ashish.nadar@siriusxm.com>
2018-02-13 14:55:56 -0600

    Initial commit

--- src/main/java/com/cv/vehicle/api/endpoint/VehicleResourceProvider.java
+++ src/main/java/com/cv/vehicle/api/endpoint/VehicleResourceProvider.java
@@ -6,8 +6,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
-import java.util.Map;
-import java.util.stream.Collector;
 import com.cv.vehicle.com.cv.vehicle.publisher.VinPublisher;
 
 @Path("/vehicles")
@@ -22,14 +20,8 @@ public class VehicleResourceProvider {
 
     @POST
     public Response postVehicleInfoToJms(VehicleAgreement vehAgreement){
-        //VinPublisher publisher = new VinPublisher();
         VinPublisher.send(vehAgreement);
-
-
-        Map<String, String> map = System.getenv();
-        map.forEach((k,v)->{
-            str = str+k+" "+v+"\n";
-        });
+        str = "Data pushed to activeMQ topic -> vin";
         Response resp = Response.ok().entity(str).build();
         return resp;
     }
